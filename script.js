e89ee6c3931f6608b5c685edae22e08faab6ddbf
liodark <122795254+liodark@users.noreply.github.com>
2023-01-17 15:50:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,164 @@
+/*let a1, a2, a3, a4, a5;
+console.log(a1)*/
+/*a1 = 7 + 2;
+a2 = 7 - 3;
+a3 = 2 * 4;
+a4 = 4 / 2;
+console.log('$  (a1),$  (a2),$  (a3),$  (a4)')*/ 
+
+/*let height = 23, width = 10;
+let s = height * width;
+console.log(s)*/
+
+/*let b1, b2, b3, b4, b5;
+b1 = 5 % 3;
+b2 = 3 % 5;
+b3 = 5 + 3;
+b4 = 5 - 3;
+b5 = 75 + 'кг';
+console.log('$  (b1),$  (b2),$  (b3),$  (b4),$  (b5)')*/
+
+/*let height, d, v;
+height = 10;
+d = 4;
+v = 3,14 * heigt * (d*d) / d
+console.log('v')*/
+
+/*let a, b;
+a = 10;
+b = 2;
+console.log(a+b, a-b, a*b, a/b)*/
+
+/*let c, d, result;
+c = 15;
+d = 2;
+console.log(result = c+d)*/
+
+/*let a, b, c;
+a = 10;
+b = 2;
+c = 5;
+console.log('a+b+c')*/
+/*
+let a, b, c;
+a = 17;
+b = 10;
+c = a + b;
+console.log('c')*/
+/*let d, c, result;
+d = 7;
+result = c + d;
+console.log('result')*/
+
+/*let сек = 60;
+let сут = 24;
+let месяц = 30;
+console.log('сек, сут, месяц')*/
+
+//№1
+
+/*let minute = 45
+if (15 >= minute && minute > 0){
+    console.log('первая')
+}
+else if (30 >= minute && minute > 15){
+    console.log('вторая')
+}
+else if (45 >= minute && minute > 30){
+    console.log('третья')
+}
+else if (59 >= minute && minute > 45){
+    console.log('четвёртая')
+}*/
+
+//№2
+
+/*let num = 5
+let result = (num == 5)?console.log(true):console.log(false);*/
+
+//№3
+
+/*let lang = 'en'
+let arr
+if (lang == 'ru'){
+    arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']
+}
+else if(lang == 'en'){
+    arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
+}
+for(i = 0;i < 7;i++){
+    console.log(arr[i])
+}*/
+
+//№4
+
+/*let input = prompt()
+if (input >= 1 && input <= 9){
+    console.log(`${input} однозначное положительное`)
+}
+else if (input >= 10 && input <= 99){
+    console.log(`${input} двузначное положительное`)
+}
+else if (input >= 100 && input <= 999){
+    console.log(`${input} трёхзначное положительное`)
+}
+else if (input >= 1000){
+    console.log(`${input} более положительное`)
+}
+else if (input <= -1 && input >= -9){
+    console.log(`${input} однозначное отрицательное`)
+}
+else if (input <= -10 && input >= -99){
+    console.log(`${input} двузначное отрицательное`)
+}
+else if (input <= -100 && input >= -999){
+    console.log(`${input} трёхзначное отрицательное`)
+}
+else if (input <= -1000){
+    console.log(`${input} более отрицательное`)
+}*/
+
+//№5
+
+/*let red
+let yellow
+if (red){
+    console.log('красный')
+}
+else if (yellow){
+    console.log('жёлтый')
+}
+else{
+    console.log('зелёный')
+}*/
+
+//№6
+
+/*let result = 1
+while(result <= 12){
+    console.log(result);
+    result++;
+}*/
+
+//№7
+
+/*for (var i_7 = 0; i_7 <= 16; i_7++) {
+          if(i_7 % 2 == 0)  {
+          console.log(i_7);
+          }
+}*/
+
+//№8
+
+let result=0
+let arr = [2, 5, 8, 3, 6]
+for (i = 0;i <15;i++)
+    result =result + arr[i];
+    console.log(result);
+}
+
+
+
+
+
+
